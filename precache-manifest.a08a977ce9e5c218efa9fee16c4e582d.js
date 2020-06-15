1462f53129eac564513997bdcbcc241becfaf79b
trishabernice <44876794+trishabernice@users.noreply.github.com>
2020-06-15 16:57:09 -0500

    Updates

---
+++ precache-manifest.a08a977ce9e5c218efa9fee16c4e582d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c5f5720714e442e09b65f9b188e684",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ae873ba38dace25825ee",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "627bb04894540427e718",
+    "url": "/portfolio/static/css/main.b4e3269c.chunk.css"
+  },
+  {
+    "revision": "ae873ba38dace25825ee",
+    "url": "/portfolio/static/js/2.68c3aa46.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.68c3aa46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "627bb04894540427e718",
+    "url": "/portfolio/static/js/main.9f9cf8f4.chunk.js"
+  },
+  {
+    "revision": "0c18a05f1fb6137df5ad",
+    "url": "/portfolio/static/js/runtime-main.24d195ed.js"
+  },
+  {
+    "revision": "97e67b61cc42312c6607be33dce74ed9",
+    "url": "/portfolio/static/media/DASB.97e67b61.png"
+  },
+  {
+    "revision": "f0bafddd7d9ad9286ecd2b039f26adc9",
+    "url": "/portfolio/static/media/IlluminateEducation.f0bafddd.png"
+  },
+  {
+    "revision": "5e9ca5d1fa829a9179852d2c5d95b0d5",
+    "url": "/portfolio/static/media/PERMIAS.5e9ca5d1.png"
+  },
+  {
+    "revision": "a726dafb14c05d9ac9b95d1c8f2e50f4",
+    "url": "/portfolio/static/media/aboutme-pic.a726dafb.JPG"
+  },
+  {
+    "revision": "41fc215333e79c58fdfe53ec22da1a82",
+    "url": "/portfolio/static/media/butterfly-background.41fc2153.png"
+  },
+  {
+    "revision": "97b04da40cde9f9fd8672e924afab3d0",
+    "url": "/portfolio/static/media/dchs-1.97b04da4.png"
+  },
+  {
+    "revision": "09aebb8630107706ba2431e9af245aad",
+    "url": "/portfolio/static/media/dchs-2.09aebb86.png"
+  },
+  {
+    "revision": "d2a47e9b4a40d94e53152dc7ef0a58df",
+    "url": "/portfolio/static/media/dchs-3.d2a47e9b.png"
+  },
+  {
+    "revision": "d065553541e0ba1b88913931ff9c1671",
+    "url": "/portfolio/static/media/donut-background.d0655535.png"
+  },
+  {
+    "revision": "70147d367f84ab281b729e7a997cee8d",
+    "url": "/portfolio/static/media/landingpace-pic.70147d36.png"
+  },
+  {
+    "revision": "ffa6212e99d17e29a16e5f702572b848",
+    "url": "/portfolio/static/media/laptop-background.ffa6212e.png"
+  },
+  {
+    "revision": "e6ec1dd5e26b58ff53af4497d6f94996",
+    "url": "/portfolio/static/media/react-logo.e6ec1dd5.jpg"
+  },
+  {
+    "revision": "f36f592d24a6bf9848fb9f41f337ed45",
+    "url": "/portfolio/static/media/shopee-logo.f36f592d.png"
+  },
+  {
+    "revision": "fb8c8fc71354d97a4b1629506df59c76",
+    "url": "/portfolio/static/media/snoopy-background.fb8c8fc7.png"
+  },
+  {
+    "revision": "583ee0f97a5c7e1aa9b249b18679ec76",
+    "url": "/portfolio/static/media/trisha-bernice-resume.583ee0f9.pdf"
+  },
+  {
+    "revision": "46bfd7a99f20d5b8fc38d8f088ed04fd",
+    "url": "/portfolio/static/media/webapp-pic.46bfd7a9.png"
+  }
+]);
