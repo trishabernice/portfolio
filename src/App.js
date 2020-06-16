2061ba26ae0fa79995380be6d6672cf500ea6cd1
trishabernice <44876794+trishabernice@users.noreply.github.com>
2020-06-15 21:38:53 -0500

    changed route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  render(){
+    return(
+      <div className="demo-big-content">
+    <Layout>
+        <Header className="header-color" title="Menu" scroll >
+            <Navigation>
+                <Link to="/" className="my-font">Home</Link>
+                <Link to="/aboutme" className="my-font">About Me</Link>
+                <Link to="/projects" className="my-font">Projects</Link>
+                <Link to="/experience" className="my-font">Experience</Link>
+                <Link to="/contact" className="my-font">Contact</Link>
+            </Navigation>
+        </Header>
+        <Drawer title="Menu">
+            <Navigation>
+                <Link to="/" className="my-font">Home</Link>
+                <Link to="/aboutme" className="my-font">About Me</Link>
+                <Link to="/projects" className="my-font">Projects</Link>
+                <Link to="/experience" className="my-font">Experience</Link>
+                <Link to="/contact" className="my-font">Contact</Link>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content" />
+            <Main></Main>
+        </Content>
+    </Layout>
+
+</div>
+
+    );
+  }
 }
 
 export default App;
