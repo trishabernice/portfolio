fffce173f7f51e562121bea50dc7476f6c482b95
trishabernice <44876794+trishabernice@users.noreply.github.com>
2020-06-16 17:13:45 -0500

    Update project-dchs.js

--- components/project-dchs.js
+++ components/project-dchs.js
@@ -1,46 +1,30 @@
 import React, { Component } from 'react';
 import { Grid, Cell } from 'react-mdl';
-import dchs1 from './comp_images/dchs-1.png';
-import dchs2 from './comp_images/dchs-2.png';
-import dchs3 from './comp_images/dchs-3.png';
+import reactLogo from './comp_images/react-logo.jpg';
+import WebAppPic from './comp_images/webapp-pic.png';
 
 
-class ProjectDCHS extends Component{
+class ProjectWebApp extends Component{
     render(){
         return(
             <div className="my-font">
                 <Grid>
                     <Cell col={6}>
-                    <div className="left-text">
+                    <div>
                         <div className="project-largeborder">
-                            <p className="xlarge-font">Dane County Humane Society Mobile Application</p>
-                            <p className="small-font">
-                             To give a little bit of background, I grew up with rabbits, dogs, and little chicks. So, it was a given that I love animals. That said, when I had to come up with a project for a Software Engineering class I took in UW-Madison, I decided to develop a mobile application for the Dane County Humane Society - a non-profit, community-supported organization that has an open admission shelter to all animals. With a team of 4 other students, the mobile application was successfully developed in about 8 weeks. 
-                            </p>
-                            <p className="small-font">The DCHS Mobile Application was designed and developed to correct problems, as well as to serve as a platform that creates an enhanced experience for both DCHS staff as well as customers. To start, my teammates and I created a Design and Specifications file to document all details regarding the application. This was the file we referred to and constantly updated as we progressed into building the application, as we practised Scrum development process. Besides, we designed the application around the Model View Presenter (MVP) Architecture, and tested the android application on a Nexus 6 API 28 phone. </p>
-                            <p className="small-font">For the back-end side of the application, we used Firebase Cloud Firestore as our database. This is where we manage data transmission, store user data and collections of documents for the animals. As for the front-end, we chose to write the UI in Dart, using the Flutter Framework. I had no knowledge about Dart/Flutter prior to the project, but it was definitely fun getting to learn and experiment with a new language!</p>
-                           
+                            <p className="xxlarge-font">Portfolio Website</p>
+                            <p className="small-font">In effort to make my summer more meaningful, I decided to develop my own web application from scratch. This web application serves as a platform for me to showcase my technical skills, creativity, and lets me share a little bit more of who I am to the world!</p>
+                            <p className="small-font">The first step I took was to dedicate two weeks into learning a new language and technology, namely - Javascript and React. After a week and a half of practical tutorials and reading on main concepts, I decided that I wanted to immediately start the project, as I know that I learn best by doing.</p>
+                            <p className="small-font">As I started developing this web application, it was a little bit of a struggle as I had to constantly go through multiple tutorial videos and StackOverflow forums simultaneously, but the thought of developing my own web app from scratch was a huge motivator and positivity boost, and I couldn’t help but to work on it for a few days straight. I love getting creative with my work, so it was incredibly fun learning to use the various components in React-MDL, in addition to experimenting with the different elements of Autodesk Sketchbook (a painting and drawing software) to design my own background images for each page on this application.</p>
+                            <p className="small-font">All this to say, there was undoubtedly a learning curve to designing and developing this web application, but it’s also amazingly fun and I’ll continue to work on and  improve this!</p>
                         </div>
                     </div>
-                    <img className="left-text" src={dchs3} width="530px" height="auto"></img>
 
                     </Cell>
 
                     <Cell col={6}>
-                        <img className="right-text" src= {dchs2} width="530px" height="300px"></img>
-                        <img className="right-text" src= {dchs1} width="530px" height="auto"></img>
-
-                        <div className="right-text">
-                            
-                            <div className="project-border">
-                            <p className="small-font">For the design, we focused on designing functionalities that do not exist on the DCHS Website, which we believed would be solving some problems for DCHS customers. These functionalities include: (1) Calendar (customers will be able to filter and check upcoming event details based on choice) (2) Notifications (alerts customers when they have upcoming events/classes) 
-                                Then, we implemented extra functionalities (such as Add, Edit, and Delete animal) that allow DCHS staff to modify animal data straight from the mobile application itself, which will update the data in our database.</p>
-                            <p className="small-font">The DCHS Mobile Application is definitely a memorable project, not only because it was the perfect combination of my passion for animals and programming, but also because I got to experiment with a new language and technology! This project also challenged both my technical skills and creativity, in addition to teaching me how to communicate effectively and efficiently with my team members.</p>
-                            <p className="small-font">Note: The DCHS Mobile Application was developed for educational purposes and is NOT available for public use.</p>
-                            </div>
-                        </div>
-
-
+                        <img className="right-img"src={reactLogo}></img>
+                        <img className="right-img"src={WebAppPic}></img>
 
                     </Cell>
                 </Grid>
@@ -49,4 +33,4 @@ class ProjectDCHS extends Component{
         );
     }
 }
-export default ProjectDCHS;
+export default ProjectWebApp;
