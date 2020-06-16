ce7868ebb68d47bca4e192a6a872d2caf3b37618
trishabernice <44876794+trishabernice@users.noreply.github.com>
2020-06-16 14:47:47 -0500

    commit

---
+++ src/components/aboutme.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import AboutMePic from './comp_images/aboutme-pic.JPG';
+
+class AboutMe extends Component{
+    render(){
+        return(
+            <div className="aboutme-grid">
+                        <div className="aboutme-text">
+                            <h1 className="my-font">About Me</h1>
+                            <p>Here is a little bit about me… </p>
+                            <p>
+                            I recently graduated (May 2020) from University of Wisconsin-Madison with a Bachelor’s of Science degree in Computer Science, along with Certificates in Entrepreneurship and Mathematics. Prior to that, I spent two years in De Anza College, California and there was where I took my first programming class - Introduction to C++. It was really challenging for me, especially in the beginning, but I can honestly say that I am so glad I persevered because the more programming classes I took, the more in love I am with what I study! 
+                            </p>
+                            <p>
+                            I look at programming as an incredible platform for me to express my creativity, and experimenting with new programming languages and technologies gives me that excitement comparable to the one I get when I see a dog walking down the street! I’m always curious and l love to explore, so I like to treat whatever I can as a learning opportunity!
+                            </p>
+                            <p>Now... things about me that aren’t related to computers and programming. I grew up with three dogs: Robin, Monie and Snoopy, and I love them with all my heart! I really really enjoy food (who doesn’t) and the outdoors, so over the summer, I typically have picnics and go on evening walks every now and then. Oh, and because of my sweet tooth, I bake about once a week, desserts ranging from cheesecakes to mochi muffins.</p>
+                            <p>Thank you so much for visiting my website, I truly hope you enjoyed going through it as much as I enjoyed making it!</p>
+                        </div>
+                        <img className="aboutme-img" src={AboutMePic}></img>
+            </div>
+        );
+    }
+}
+export default AboutMe;
---
+++ src/components/projects.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { Grid, Cell, Card, CardTitle, CardText, CardMenu, CardActions, Button, IconButton } from 'react-mdl';
+import { Navigation } from 'react-mdl';
+import { Link } from 'react-router-dom';
+
+
+class Projects extends Component{
+    render(){
+        return(
+            <div className="my-font">
+                <Grid className="projectsbackground-grid">
+                    {/* PROJECT #1*/}
+                        <Card shadow={5} style={{height:'50vh', width:'40vw', margin:'auto'}}>
+                            <CardTitle style = {{color: 'white', height: '25vh', background: 'url(https://mljm66nyxdyi.i.optimole.com/P29XpA-_CVPWVwh/w:467/h:399/q:auto/https://sceel.io/wp-content/uploads/2019/10/Flutter-Logo-2.png) center / cover'}}>
+                                DCHS Mobile Application
+                            </CardTitle>
+                            <CardText className="small-font">
+                            The Dane County Humane Society Mobile Application is modeled after the non-profit organization - Dane County Humane Society. This mobile application was designed for efficiency, and has two different sides to it: (1) DCHS employees (2) Customers. The DCHS Mobile Application serves to provide all services the website already provides, in addition to introducing an array of new functionalities such as a Calendar, a Notifications tab, etc. 
+                            </CardText>
+                            <CardActions border margin="2vw">
+                                <Button>
+                                <Navigation>
+                                    <Link to="/project-dchs" className="my-font">Go to Project</Link>
+                                </Navigation>
+                                </Button>
+                            </CardActions>
+                        </Card>
+
+                    {/* PROJECT #2*/}
+                        <Card shadow={5} style={{height:'50vh', width:'40vw', margin:'auto'}}>
+                            <CardTitle style = {{color: 'white', height: '25vh', background: 'url(https://miro.medium.com/max/1200/0*JWL-dYH8mOxe9EGb.png) center / cover'}}>
+                                Portfolio Website
+                            </CardTitle>
+                            <CardText className="small-font">
+                            This Portfolio Website is my first project for Summer 2020. The purpose of this application is to act as a platform to showcase my technical skills and creativity! I love learning and experimenting with new languages and technology, and this web app allowed me to play with React, React-MDL, React Router, CSS and Javascript. Designing this application was incredibly fun and I’ll definitely come back to improve the nitty gritty of this project!
+                            </CardText>
+                            <CardActions border margin="2vw">
+                                <Button>
+                                <Navigation>
+                                    <Link to="/project-webapp" className="my-font">Go to Project</Link>
+                                </Navigation>
+                                </Button>
+                            </CardActions>
+                        </Card>
+
+
+
+
+                        </Grid>
+            </div>
+        );
+    }
+}
+export default Projects;
+
